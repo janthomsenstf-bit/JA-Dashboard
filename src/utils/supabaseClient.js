1a160b090f52134a69af4a841f08d2cfd5aed9f5
Jan Thomsen <jan.thomsen.stf@gmail.com>
2026-04-06 15:01:27 +0200

    Fix Supabase anon key (use correct JWT format)

--- src/utils/supabaseClient.js
+++ src/utils/supabaseClient.js
@@ -2,5 +2,5 @@ import { createClient } from '@supabase/supabase-js'
 
 export const supabase = createClient(
   'https://wtvijpdfdfyagmiwwnlw.supabase.co',
-  'sb_publishable_xPPdiaiZ4XSktjrzSta4xw_D3ooGZ5q',
+  'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Ind0dmlqcGRmZGZ5YWdtaXd3bmx3Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzU0NDY2OTYsImV4cCI6MjA5MTAyMjY5Nn0.Kfcb-b9vJmEL6CCxPrxwlkqMM-mUcM_J355zMhp143g',
 )
